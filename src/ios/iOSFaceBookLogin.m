49dcba44020dedfad819e89783e091e8a4171826
Nick Barth <nickbarth@live.com>
2014-08-09 17:33:20 -0600

    Update iOSFaceBookLogin.m

--- src/ios/iOSFaceBookLogin.m
+++ src/ios/iOSFaceBookLogin.m
@@ -6,9 +6,9 @@
 
 - (void)iOSFaceBookLogin:(CDVInvokedUrlCommand *)command {
     self.callbackId = command.callbackId;
-
+    
     NSArray *permissionsArray = @[ @"user_friends", @"user_about_me", @"user_relationships", @"user_birthday", @"user_location"];
-
+    
     [PFFacebookUtils logInWithPermissions:permissionsArray block:^(PFUser *user, NSError *error) {
         if (user) {
             NSString *result = [NSString stringWithFormat: @"{ \"error\": false, \"sessionToken\": \"%@\" }", user.sessionToken];
@@ -16,7 +16,7 @@
             NSString* javaScript = [pluginResult toSuccessCallbackString:self.callbackId];
             [self writeJavascript:javaScript];
         } else if (error) {
-            NSString *result = [NSString stringWithFormat: @"{ \"error\": \"%@\" }", @"Please ensure you enable Beep in your iOS Facebook Settings"];
+            NSString *result = @"{ \"error\": \"Please ensure you enable Beep in your iOS Facebook Settings\" }";
             CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:result];
             NSString* javaScript = [pluginResult toSuccessCallbackString:self.callbackId];
             [self writeJavascript:javaScript];
